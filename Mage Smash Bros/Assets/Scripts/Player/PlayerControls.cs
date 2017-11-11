540c57a21af154e5378d3991b16f44801997be09
Nicolas Fischer <nicolas.fischer@hs-augsburg.de>
2017-11-11 15:44:11 +0100

    Changes 1.Added material for player 1 and player 2 2.Tried something at player movement - not yet working 3.simple interface added

--- Mage Smash Bros/Assets/Scripts/Player/PlayerControls.cs
+++ Mage Smash Bros/Assets/Scripts/Player/PlayerControls.cs
@@ -7,40 +7,87 @@ public class PlayerControls : MonoBehaviour {
     private float forwardInput;
     private Rigidbody rb;
     private Animator playerAnimator;
-
+    public GameObject playerModel;
+    public int movementType = 2;
     public float moveAcceleration = 4;
+    //Type 2
+    public float maxSpeed = 5;
+    public float currentSpeed;
+    public float acceleration = 2;
+
+    
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        GetInput();
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();        
+    }
 
     private void FixedUpdate()
     {
         Move();
+        Turn();
         SetAnimator();
     }
 
     private void GetInput()
     {
         forwardInput = Input.GetAxis("Horizontal");
-        Debug.Log(forwardInput);
 
         if (Input.GetButton("Jump"))
         {
-            Debug.Log("jump");
+            //do Jump
         }
     }
 
     private void Move()
     {
-        rb.velocity = new Vector3(forwardInput * moveAcceleration, rb.velocity.y, 0);
+        if (movementType == 1)
+        {
+            rb.velocity = new Vector3(forwardInput * moveAcceleration, rb.velocity.y, 0);
+        }
+        else if (movementType == 2)
+        {
+            currentSpeed = currentSpeed + forwardInput * acceleration;
+            if (currentSpeed >= maxSpeed)
+            {
+                currentSpeed = maxSpeed;
+            }
+            else if (currentSpeed <= -maxSpeed)
+            {
+                currentSpeed = -maxSpeed;
+            }
+            rb.velocity = new Vector3(currentSpeed, rb.velocity.y, 0);
+        }
+        if (forwardInput == 0)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y,0);
+        }                           
+    }
+
+    private void Turn()
+    {
+        if (movementType == 1)
+        {
+            if (forwardInput > 0)
+            {
+                playerModel.transform.eulerAngles = new Vector3(0, 90, 0);
+            }
+            else if (forwardInput < 0)
+            {
+                playerModel.transform.eulerAngles = new Vector3(0, -90, 0);
+            }
+        }    
+        else if (movementType == 2)
+        {
+
+        }    
     }
 
     private void SetAnimator()
